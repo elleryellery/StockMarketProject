dc1e21240d68e1b209f7b4897f08589d65a031d0
McDaniel <S1736460@online.houstonisd.org>
2024-09-26 14:33:15 -0500

    attempt #2 to fix dotenv error

--- StockHandler.py
+++ StockHandler.py
@@ -1,6 +1,5 @@
 import os 
 import random
-from dotenv import load_dotenv
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
